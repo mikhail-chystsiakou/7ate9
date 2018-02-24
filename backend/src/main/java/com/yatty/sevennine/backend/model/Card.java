20ad2e1ad63cf6e699175eeee35ac4329c10eda5
Dmitry Golyota <goletadm@gmail.com>
2018-02-24 23:14:02 +0300

    Card and deck model classes added

---
+++ backend/src/main/java/com/yatty/sevennine/backend/model/Card.java
@@ -0,0 +1,37 @@
+package com.yatty.sevennine.backend.model;
+
+/**
+ * Represents card model.
+ *
+ * @version 024/02/18
+ * @author Dmitry
+ */
+public class Card {
+    private int largeNumeral;
+    private int smallNumeral;
+    private Color color;
+
+    public static enum Color {
+        RED,
+        GREEN,
+        BLUE
+    }
+
+    public Card(int ln, int sn, Color col) {
+        largeNumeral = ln;
+        smallNumeral = sn;
+        color = col;
+    }
+
+    public int getLargeNumeral() {
+        return largeNumeral;
+    }
+
+    public int getSmallNumeral() {
+        return smallNumeral;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
---
+++ backend/src/main/java/com/yatty/sevennine/backend/model/Deck.java
@@ -0,0 +1,50 @@
+package com.yatty.sevennine.backend.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+/**
+ * Represents deck of cards model.
+ *
+ * @version 024/02/18
+ * @author Dmitry
+ */
+public class Deck {
+    private ArrayList<Card> cardList;
+    private Random rnd;
+    private int playersNumber;      // от 2 до 4 игроков
+
+    private static final int DEFAULT_CARDS_AMOUNT = 18;     //  в полной колоде 73 карты, т.е. делим на максимальное число игроков(4) и получаем 18 карт на одного игрока
+
+    public Deck() {
+        rnd = new Random(System.currentTimeMillis());
+        cardList = new ArrayList<>();
+    }
+
+    public void generate(int pn) {
+        playersNumber = pn;
+
+        Card.Color[] colors = Card.Color.values();
+        for (int i = 0; i < playersNumber * DEFAULT_CARDS_AMOUNT + 1; i++) {
+            cardList.add(new Card(rnd.nextInt(11 - 1), rnd.nextInt(4-1), colors[rnd.nextInt(3)]));
+        }
+    }
+
+    public void shuffle() {
+        for (int i = 0; i < playersNumber * DEFAULT_CARDS_AMOUNT; i++)
+            Collections.swap(cardList, rnd.nextInt(), rnd.nextInt());
+    }
+
+    public ArrayList<Card> pullCards() {
+        ArrayList<Card> playerList = new ArrayList<>();
+        for (int i = 0; i < DEFAULT_CARDS_AMOUNT; i++) {
+            playerList.add(cardList.remove(0));
+        }
+        return playerList;
+    }
+
+    public Card getCard() {
+        return cardList.remove(rnd.nextInt(cardList.size()));
+    }
+}
