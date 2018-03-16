93639e5d28dbb77bf21c27772ae2b09a7a187231
zoxal <zzoxall@gmail.com>
2018-03-16 23:55:33 +0300

    Card equals and hashcode for proper deletion from deck

--- shared-api/src/main/java/com/yatty/sevennine/api/Card.java
+++ shared-api/src/main/java/com/yatty/sevennine/api/Card.java
@@ -1,5 +1,6 @@
 package com.yatty.sevennine.api;
 
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -33,7 +34,21 @@ public class Card {
         Random random = new Random(System.currentTimeMillis());
         return new Card(random.nextInt(10) + 1, random.nextInt(3) + 1);
     }
-
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return value == card.value &&
+                modifier == card.modifier;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, modifier);
+    }
+    
     @Override
     public String toString() {
         return value + "±" + modifier;
